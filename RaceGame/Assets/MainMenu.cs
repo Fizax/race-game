18d6117d6771d2517bc9180aeb4156538cebbdb5
Unknown <feyza@keles.nl>
2019-10-02 15:59:35 +0200

    color start

--- RaceGame/Assets/MainMenu.cs
+++ RaceGame/Assets/MainMenu.cs
@@ -16,4 +16,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
